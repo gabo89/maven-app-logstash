3a19474b3c23105d8f490116a51ecaddfd5874c0
gabocuadros <gabrielcuadros@gmail.com>
2020-07-24 01:41:17 -0500

    fixed logback

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,40 @@
 package com.example.demo;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication implements CommandLineRunner {
+    
+    
+        private static Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+  
 	public static void main(String[] args) {
+                LOG.info("STARTING THE APPLICATION");
 		SpringApplication.run(DemoApplication.class, args);
+                LOG.info("APPLICATION FINISHED");
 	}
+        
+    @Autowired
+    ApplicationContext applicationContext;
+        
+    @Override
+    public void run(String... args)
+    {
+        System.out.println("------------------------------------------");
+        System.out.println("check beans loaded  by springboot:");
+    	System.out.println("------------------------------------------");
+        LOG.info("APPLICATION SHOWING BEANS");
 
+	String[] beanNames = applicationContext.getBeanDefinitionNames();
+	Arrays.sort(beanNames);
+	for (String beanName : beanNames) {
+            System.out.println(beanName);	} 
+    }
 }
